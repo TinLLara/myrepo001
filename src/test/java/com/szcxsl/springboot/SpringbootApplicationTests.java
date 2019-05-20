fa114646bc8d93015c8a1554bee2d952022c884f
“tin” <286185834@qq.com>
2019-05-20 23:49:59 +0800

    spring boot日志配置

--- src/test/java/com/szcxsl/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/szcxsl/springboot/SpringbootApplicationTests.java
@@ -3,6 +3,8 @@ package com.szcxsl.springboot;
 import com.szcxsl.springboot.dto.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -20,6 +22,8 @@ public class SpringbootApplicationTests {
     @Autowired
     ApplicationContext applicationContext;
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void testBeans() {
         boolean b = applicationContext.containsBean("userService");
@@ -28,5 +32,17 @@ public class SpringbootApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(user);
+    }
+
+    @Test
+    public void testLogs() {
+        /**
+         * spring boot 默认日志级别为info，可在配置文件进行调整(可指包)，其他默认使用info级别日志：root日志
+         */
+        logger.trace("trace日志");
+        logger.debug("debug日志");
+        logger.info("info日志");
+        logger.warn("warn日志");
+        logger.error("error日志");
     }
 }
